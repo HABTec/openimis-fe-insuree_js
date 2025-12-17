09f2aaa67e1f013677c1e64ad865a7047805c3ff
bereketassefa <bereket.a.gedefaw@gmail.com>
2025-12-17 14:45:24 +0300

    fix: fixing activate button isssue for family and insurees

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -24,21 +24,17 @@ export const isValidInsuree = (insuree, modulesManager) => {
     "insureeForm.isInsureePhotoRequired",
     false,
   );
-
   const isInsureeStatusRequired = modulesManager.getConf("fe-insuree", "insureeForm.isInsureeStatusRequired", false);
-
   if (isInsureeFirstServicePointRequired && !insuree.healthFacility) return false;
   if (insuree.validityTo) return false;
-  if (insuree.family && !insuree.relationship) return false;
+  if (insuree.family && !insuree.head && !insuree.relationship) return false;
   if (!insuree.lastName) return false;
   if (!insuree.middleName) return false;
   if (!insuree.otherNames) return false;
   if (!insuree.dob) return false;
   if (!insuree.gender || !insuree.gender?.code) return false;
-  if (!!insuree.photo && (!insuree.photo.date || !insuree.photo.officerId)) return false;
   if (isInsureeStatusRequired && !insuree.status) return false;
   if (isInsureePhotoRequired && !insuree.photo) return false;
-  if (!!insuree.status && insuree.status !== INSUREE_ACTIVE_STRING && (!insuree.statusDate || !insuree.statusReason)) return false;
 
   return true;
 };
